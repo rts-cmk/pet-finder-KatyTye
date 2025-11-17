66ac3a35adef0e95d2c64c5a64adb59c5be0fb08
David Majholt <d.boy.dragoz@gmail.com>
2025-11-17 10:36:05 +0100

    feat: enhance Intro page with image, text, and navigation; add styles for layout

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,8 +1,36 @@
+import AnimalImage from "../assets/images/Animal.svg"
+import { Link, useNavigate } from "react-router"
+import "../styles/_intro.sass"
+import { useEffect } from "react"
+
 function Intro() {
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		const introStatus = Boolean(localStorage.getItem("introCompleted")) || false
+		if (introStatus) {
+			navigate("/home")
+		}
+	}, [])
 
 	return (
-		<>
-		</>
+		<main className="main-content intro-content">
+			<img src={AnimalImage} alt="A image of a cat in the wild" className="intro-content__image"></img>
+
+			<section className="intro-content__section">
+				<h2 className="intro-content__title">
+					My Pets
+				</h2>
+
+				<p className="intro-content__text intro-desc">
+					Taking care of a pet is my favorite, it helps me to gaimr stress and fatigue.
+				</p>
+			</section>
+
+			<Link to={{ pathname: "/home", search: "?intro=completed" }} className="intro-content__button">
+				Skip
+			</Link>
+		</main>
 	)
 }
 
