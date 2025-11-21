3ccdb452a7fa35de2f469e21d7f9ebf437607ae4
David Majholt <d.boy.dragoz@gmail.com>
2025-11-21 11:15:54 +0100

    feat: updated error page and styles for error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,19 @@
+import { Link } from "react-router"
+
 function Error() {
 
 	return (
-		<>
-			<h2>
-				Error Page
+		<main className="main-content error-content">
+			<h2 className="error-content__error-code">
+				404
 			</h2>
-		</>
+			<p className="error-content__error-details">
+				Could not find the page, click below for going back home.
+			</p>
+			<Link className="error-content__error-button" to={"/"}>
+				Home
+			</Link>
+		</main>
 	)
 }
 
