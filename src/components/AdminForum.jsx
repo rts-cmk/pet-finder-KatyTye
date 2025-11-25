c7595fd2364175c07c17ac7a8505a06f743c48be
David Majholt <d.boy.dragoz@gmail.com>
2025-11-25 09:46:17 +0100

    feat: changed admin forum so every input field is required

--- src/components/AdminForum.jsx
+++ src/components/AdminForum.jsx
@@ -18,7 +18,8 @@ export default function () {
 
 			<label htmlFor="categorie" className="admin-content__form-label">
 				Type:
-				<select name="categorie" id="type" className="admin-content__form-input">
+				<select name="categorie" id="type" className="admin-content__form-input"
+					required>
 					<option value="">Click to select pet type</option>
 					<option value="dogs">Dog</option>
 					<option value="cats">Cat</option>
@@ -29,7 +30,8 @@ export default function () {
 
 			<label htmlFor="gender" className="admin-content__form-label">
 				Gender:
-				<select name="gender" id="gender" className="admin-content__form-input">
+				<select name="gender" id="gender" className="admin-content__form-input"
+					required>
 					<option value="">Click to select pet gender</option>
 					<option value="male">Male</option>
 					<option value="female">Female</option>
@@ -37,29 +39,33 @@ export default function () {
 				</select>
 			</label>
 
-			<label htmlFor="breed" className="admin-content__form-label">
+			<label htmlFor="breed" className="admin-content__form-label" >
 				Breed:
-				<input type="text" className="admin-content__form-input" name="breed" id="breed"></input>
+				<input type="text" className="admin-content__form-input" name="breed" id="breed"
+					required />
 			</label>
 
 			<label htmlFor="location" className="admin-content__form-label">
 				Location:
-				<input type="text" className="admin-content__form-input" name="location" id="location"></input>
+				<input type="text" className="admin-content__form-input" name="location" id="location"
+					required></input>
 			</label>
 
 			<label htmlFor="short_description" className="admin-content__form-label">
 				Short Description:
-				<input type="text" className="admin-content__form-input" name="short_description" id="short_description"></input>
+				<input type="text" className="admin-content__form-input" name="short_description" id="short_description"
+					required></input>
 			</label>
 
 			<label htmlFor="description" className="admin-content__form-label">
 				Long Description:
-				<textarea className="admin-content__form-input long-text" name="description" id="description"></textarea>
+				<textarea className="admin-content__form-input long-text" name="description" id="description"
+					required></textarea>
 			</label>
 
 			<p className="admin-content__form-output">{message}</p>
 
-			<button className="admin-content__form-button">Submit New Pet</button>
+			<button className="admin-content__form-button" type="submit">Submit New Pet</button>
 		</form>
 	)
 }
